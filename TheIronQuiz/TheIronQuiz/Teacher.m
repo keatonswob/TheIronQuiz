58f216fa727ad5fe6d45225aaa9ff7c79418a31e
Randall Lee <randallee@gmail.com>
2015-04-08 18:10:19 -0400

    Added a Teacher Model Class in an attempt to make some mock data to play with

---
+++ TheIronQuiz/TheIronQuiz/Teacher.m
@@ -0,0 +1,13 @@
+//
+//  Teacher.m
+//  TheIronQuiz
+//
+//  Created by Apple on 4/8/15.
+//  Copyright (c) 2015 The Iron Yard. All rights reserved.
+//
+
+#import "Teacher.h"
+
+@implementation Teacher
+
+@end
