d146c3f75b6981540489f0192a1bd258dd555cf4
Randall Lee <randallee@gmail.com>
2015-04-08 11:36:57 -0400

    added segues and another modalVC class

---
+++ TheIronQuiz/TheIronQuiz/TopicModalTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  TopicModalTableViewController.h
+//  TheIronQuiz
+//
+//  Created by Apple on 4/8/15.
+//  Copyright (c) 2015 The Iron Yard. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TopicModalTableViewController : UITableViewController
+
+@end
