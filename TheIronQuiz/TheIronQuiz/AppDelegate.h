2900aad01e913dd839d5649200f43d40094b41d2
Keaton Swoboda <keatonswoboda@Keatons-MacBook-Pro.local>
2015-04-08 10:05:03 -0400

    initial commit

---
+++ TheIronQuiz/TheIronQuiz/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TheIronQuiz
+//
+//  Created by Keaton Swoboda on 4/8/15.
+//  Copyright (c) 2015 The Iron Yard. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TheIronQuiz/TheIronQuiz/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TheIronQuiz
+//
+//  Created by Keaton Swoboda on 4/8/15.
+//  Copyright (c) 2015 The Iron Yard. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
